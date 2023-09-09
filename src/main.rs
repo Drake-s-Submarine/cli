c95967e1825a1cbeabf4d3d298107d2a95765310
Drake Main <drakemain.dev@gmail.com>
2023-09-09 15:39:33 -0700

    Use seralized command format
    See https://github.com/Drake-s-Submarine/system_controller/commit/93641aaae8b34c00fd8fae4aac81dcae5249c02d

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use structopt::StructOpt;
 use std::os::unix::net::UnixStream;
 use std::io::Write;
 
+// TODO: create shared definitions
+
+const BALLAST_ID: u8 = 0x0;
+const BUFFER_BYTE_LEN: usize = 16;
+
 #[derive(StructOpt, Debug)]
 #[structopt(name = "Sub Command Utility",
             about = "A cli for testing submarine commands.")]
@@ -10,37 +15,55 @@ struct Opt {
 }
 
 fn main() {
-    println!("Reading args");
     let opt = Opt::from_args();
-    println!("Binding socket");
     let mut socket = UnixStream::connect("/tmp/sub_cmd_socket")
         .expect("Failed to connect to socket.");
-    println!("Socket bound!");
     
-    let cmd = match opt.cmd.as_str() {
-        "enable" => "EnableMotor",
-        "disable" => "DisableMotor",
+    let cmd: [u8; BUFFER_BYTE_LEN] = match opt.cmd.as_str() {
+        "enable" => create_ballast_command(1).unwrap(),
+        "disable" => create_ballast_command(0).unwrap(),
+        "bad-buf-start" => {
+            let mut buf = create_ballast_command(0).unwrap();
+            buf[0] = 0xB;
+            buf
+        },
+        "bad-buf-end" => {
+            let mut buf = create_ballast_command(0).unwrap();
+            buf[BUFFER_BYTE_LEN - 1] = 0xE;
+            buf
+        },
+        "bad-buf-mod" => {
+            let mut buf = create_ballast_command(0).unwrap();
+            buf[1] = 0xF;
+            buf
+        },
         _ => {
             println!("Malformed command: {}", opt.cmd);
-            return
+            return;
         }
     };
 
-    println!("Command is: {}", cmd);
-
-    socket.write_all(cmd.as_bytes())
+    socket.write_all(&cmd)
         .expect("Failed to write command to socket.");
+}
 
-    //for stream in socket.incoming() {
-    //    println!("TEST");
-    //    match stream {
-    //        Ok(mut stream) => {
-    //            println!("Writing command");
-    //            stream.write(cmd.as_bytes()).expect("Failed to write command to socket.");
-    //            println!("Done");
-    //            stream.shutdown(Shutdown::Write).expect("Failed to shut down socket.");
-    //        }
-    //        Err(e) => println!("Failed to connect to stream: {}", e)
-    //    }
-    //}
+fn create_command_buffer_template() -> [u8; BUFFER_BYTE_LEN] {
+    let mut buf: [u8; BUFFER_BYTE_LEN] = [0; BUFFER_BYTE_LEN];
+
+    buf[0] = 0xA;
+    buf[BUFFER_BYTE_LEN - 1] = 0xF;
+
+    buf
+}
+
+fn create_ballast_command(state: u8) -> Result<[u8; BUFFER_BYTE_LEN], ()> {
+    let mut buf = create_command_buffer_template();
+
+    if state < 3 {
+        buf[2] = state;
+        buf[1] = BALLAST_ID;
+        Ok(buf)
+    } else {
+        Err(())
+    }
 }
